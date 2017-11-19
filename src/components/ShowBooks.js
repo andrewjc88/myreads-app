0f2a4578356ddce1deb1fc9a0ded99a91403b973
andrewjc88 <andrewjc88@gmail.com>
2017-11-19 10:58:57 -0800

    fix gh-pages hosting CNAME

--- src/components/ShowBooks.js
+++ src/components/ShowBooks.js
@@ -1,45 +1,40 @@
 import React from 'react'
 
-const ShowBooks = ({onChangeShelf, books, shelf}) => ({
+const ShowBooks = ({onChangeShelf, books, shelf}) => {
 
-  render() {
+  return (
 
-    const { onChangeShelf, books, shelf} = this.props
+    <ol className="books-grid">
 
-    return (
-        <ol className="books-grid">
+      {books.map((book) => (           
+        ((shelf === book.shelf) || (shelf === '')) && (
+          <li key={book.id}>
+            <div className="book">
+              <div className="book-top">
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                <div className="book-shelf-changer">
+                {(book.shelf === undefined) && (book.shelf = '')}
 
-          {books.map((book) => (           
-            ((shelf === book.shelf) || (shelf === '')) && (
-              <li key={book.id}>
-              {console.log(book.shelf)}
-                <div className="book">
-                  <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                    <div className="book-shelf-changer">
-                    {(book.shelf === undefined) && (book.shelf = '')}
+                  <select value={book.shelf} onChange={event => onChangeShelf(book, event.target.value)}>
 
-                      <select value={book.shelf} onChange={event => onChangeShelf(book, event.target.value)}>
+                    <option value="none" disabled>Move to...</option>
+                    <option value="currentlyReading" >Currently Reading</option>
+                    <option value="wantToRead" >Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value=''>None</option>
 
-                        <option value="none" disabled>Move to...</option>
-                        <option value="currentlyReading" >Currently Reading</option>
-                        <option value="wantToRead" >Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value=''>None</option>
-
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.title}</div>
-                  <div className="book-authors">{book.auther}</div>
+                  </select>
                 </div>
-              </li>
-            )
-          ))}
+              </div>
+              <div className="book-title">{book.title}</div>
+              <div className="book-authors">{book.auther}</div>
+            </div>
+          </li>
+        )
+      ))}
+    </ol>
 
-        </ol>
-    )
-  }
-})
+  );
+};
 
 export default ShowBooks
--- src/components/ShowShelves.js
+++ src/components/ShowShelves.js
@@ -2,51 +2,48 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import changeCase from 'change-case'
 import ShowBooks from './ShowBooks'
-// import sortBy from 'sort-by'
 
-const ShowShelves = ({}) => ({
+const ShowShelves = ({}) => {
 
-  render() {
+  const { books } = this.props
 
-    const { books } = this.props
+  const bookShelves = books.map(book => book = book.shelf )
+  const shelves = [...new Set(bookShelves)]
+  shelves.sort();
 
-    const bookShelves = books.map(book => book = book.shelf )
-    const shelves = [...new Set(bookShelves)]
+  return (
+    <div>
+      <div className="list-books">
 
-    return (
-      <div>
-          <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
 
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
+        {shelves.map((shelf) => ( 
 
-            {shelves.map((shelf) => ( 
-
-            <div className="list-books-content" key={shelf}>
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">{changeCase.titleCase(shelf)}</h2>
-                  <div className="bookshelf-books">
-                    <ShowBooks
-                      onChangeShelf={ this.props.onChangeShelf }
-                      shelf={ shelf }
-                      books={ books }
-                    />
-                  </div>
-                </div>            
-              </div>
-            </div>
-            ))} 
-
-            <div className="open-search">
-              <Link to="/Search">Add a book</Link>
+          <div className="list-books-content" key={shelf}>
+            <div>
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">{changeCase.titleCase(shelf)}</h2>
+                <div className="bookshelf-books">
+                  <ShowBooks
+                    onChangeShelf={ this.props.onChangeShelf }
+                    shelf={ shelf }
+                    books={ books }
+                  />
+                </div>
+              </div>            
             </div>
           </div>
-      </div>
+        ))};
 
-    )
-  }
-})
+        <div className="open-search">
+            <Link to="/Search">Add a book</Link>
+        </div>
+
+      </div>
+    </div>
+  )
+}
   
 export default ShowShelves
